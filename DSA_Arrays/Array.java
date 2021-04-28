19a356d176a6f531471bbd6a6217bb7d8eedbb22
joshiadvait8 <joshiadvait8@gmail.com>
2021-04-28 16:01:51 +0530

    arrays++

--- DSA_Arrays/Array.java
+++ DSA_Arrays/Array.java
@@ -1,10 +1,67 @@
 package DSA_Arrays;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Array {
     int arr[];
     int count;
     public Array(int length){
         arr=new int[length];
+    }
+
+    public void insertAt(int item,int index){
+        if(index<0 || index >=count)
+            throw new IllegalArgumentException();
+        //shift items after given index to right
+        count++;
+        int newItems[] = new int[count];
+        int j=0;
+        for(int i=0;i<count;i++){
+            if(i==index){
+                newItems[i]=item;
+                continue;
+            }
+//            if(i>index)
+//                newItems[i+1] = arr[i];
+            else
+                newItems[i]=arr[j++];
+
+        }
+
+        arr=newItems;
+
+    }
+
+    public void reverse(){
+        int[] newArr = new int[count];
+        int j=0;
+        for(int i=count-1;i>=0;i--){
+            newArr[j++]=arr[i];
+        }
+        arr = newArr;
+    }
+
+    public List<Integer> intersect(int otherArr[]){
+        //O(n^2) time
+        List<Integer> commonItems = new ArrayList<>();
+        for(int i=0;i<count;i++){
+            for(int j=0;j<otherArr.length;j++){
+                if(arr[i]==otherArr[j])
+                    commonItems.add(arr[i]);
+
+            }
+        }
+        return commonItems;
+    }
+    public int max(){
+        //O(n) time
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<count;i++){
+            if(arr[i]>=max)
+                max = arr[i];
+        }
+        return max;
     }
     public void insert(int item){
         //if array is full we will resize it
--- DSA_Arrays/Main.java
+++ DSA_Arrays/Main.java
@@ -11,21 +11,31 @@ public class Main {
         a.insert(3);
         a.insert(4);
         a.insert(5);
-        a.removeAt(2);
+//        a.removeAt(2);
+        System.out.println(a.max());
+        int[] b = {1,10};
+        System.out.println(a.intersect(b));
         a.print();
-        a.removeAt(0);
+        a.reverse();
         a.print();
-        System.out.println(a.indexOf(3));
+        a.insertAt(10,3);
+        a.print();
+
+
+//        a.print();
+//        a.removeAt(0);
+//        a.print();
+//        System.out.println(a.indexOf(3));
 
         //Innbuilt dyanmic array
         //arraylist --> 50% grow
         //vectors -->100% grow
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(10);
-        list.remove(0);
-        list.toArray();
-        System.out.println(list.toString());
+//        ArrayList<Integer> list = new ArrayList<>();
+//        list.add(1);
+//        list.add(10);
+//        list.remove(0);
+//        list.toArray();
+//        System.out.println(list.toString());
 
     }
 }
